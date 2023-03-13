26188ddb92d65bde9e09a9b378d095f3950c1aac
onalcan <onal@liftric.com>
2023-03-13 21:58:23 +0100

    basic shapes

--- app/src/main/java/com/canonal/canvascompose/MainActivity.kt
+++ app/src/main/java/com/canonal/canvascompose/MainActivity.kt
@@ -3,41 +3,75 @@ package com.canonal.canvascompose
 import android.os.Bundle
 import androidx.activity.ComponentActivity
 import androidx.activity.compose.setContent
-import androidx.compose.foundation.layout.fillMaxSize
-import androidx.compose.material.MaterialTheme
-import androidx.compose.material.Surface
-import androidx.compose.material.Text
+import androidx.compose.foundation.Canvas
+import androidx.compose.foundation.layout.padding
+import androidx.compose.foundation.layout.size
 import androidx.compose.runtime.Composable
 import androidx.compose.ui.Modifier
-import androidx.compose.ui.tooling.preview.Preview
-import com.canonal.canvascompose.ui.theme.CanvasComposeTheme
+import androidx.compose.ui.geometry.Offset
+import androidx.compose.ui.geometry.Size
+import androidx.compose.ui.graphics.Brush
+import androidx.compose.ui.graphics.Color
+import androidx.compose.ui.graphics.drawscope.Stroke
+import androidx.compose.ui.unit.dp
 
 class MainActivity : ComponentActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContent {
-            CanvasComposeTheme {
-                // A surface container using the 'background' color from the theme
-                Surface(
-                    modifier = Modifier.fillMaxSize(),
-                    color = MaterialTheme.colors.background
-                ) {
-                    Greeting("Android")
-                }
-            }
+            MyCanvas()
         }
     }
 }
 
 @Composable
-fun Greeting(name: String) {
-    Text(text = "Hello $name!")
-}
-
-@Preview(showBackground = true)
-@Composable
-fun DefaultPreview() {
-    CanvasComposeTheme {
-        Greeting("Android")
+fun MyCanvas() {
+    Canvas(
+        modifier = Modifier
+            .padding(20.dp)
+            .size(300.dp)
+    ) {
+        drawRect(
+            color = Color.Black,
+            size = size
+        )
+        drawRect(
+            color = Color.Red,
+            topLeft = Offset(x = 100f, y = 100f),
+            size = Size(width = 100f, height = 100f),
+            style = Stroke(
+                width = 3.dp.toPx()
+            )
+        )
+        drawCircle(
+            brush = Brush.radialGradient(
+                colors = listOf(Color.Red, Color.Yellow),
+                center = center,
+                radius = 100f
+            ),
+            radius = 100f
+        )
+        drawArc(
+            color = Color.Green,
+            startAngle = 0f,
+            sweepAngle = 270f,
+            useCenter = false,
+            topLeft = Offset(x = 100f, y = 500f),
+            size = Size(width = 200f, height = 200f),
+            style = Stroke(
+                width = 3.dp.toPx()
+            )
+        )
+        drawOval(
+            color = Color.Magenta,
+            topLeft = Offset(x = 500f, y = 100f),
+            size = Size(width = 200f, height = 300f)
+        )
+        drawLine(
+            color = Color.Cyan,
+            start = Offset(x = 300f, y = 700f),
+            end = Offset(x = 700f, y = 700f),
+            strokeWidth = 5.dp.toPx()
+        )
     }
 }
